10489fe8b3e21194f5c024b72a16ad9d561b1985
Julian krochmal <jkrochmal.ext@andromedalatam.com>
2021-09-09 16:45:20 -0300

    adding quote components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import './App.css';
+import styled from '@emotion/styled';
+import { useState } from 'react';
+import Quote from './components/Quote';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  margin-top: 7rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 
 function App() {
+
+  const [phrase, setPhrase] = useState({})
+
+  const apiCall = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    const quote = await api.json()
+    setPhrase(quote[0])
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      <Quote />
+
+      <Button
+        onClick={apiCall}
+      >
+        Get phrase
+      </Button>
+
+    </Container>
   );
 }
 
