57e7dba8bb597fc4580b74f7df1cc95a56bb5c3a
julikroch <juliankrochmal@gmail.com>
2021-09-10 17:14:06 -0300

    separting style.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,6 @@
-import styled from '@emotion/styled';
 import { useState } from 'react';
 import Quote from './components/Quote';
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  padding-top: 5rem;
-  flex-direction: column;
-`;
-
-const Button = styled.button`
-  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
-  background-size: 300px;
-  font-family: Arial, Helvetica, sans-serif;
-  color: white;
-  margin-top: 7rem;
-  padding: 1rem 3rem;
-  font-size: 2rem;
-  border: 2px solid black;
-`;
+import { Container, Button } from './style';
 
 export type Phrase = {
   author: string,
---
+++ src/style.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+export const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: white;
+  margin-top: 7rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
